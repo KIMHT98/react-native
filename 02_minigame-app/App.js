a3cd185e11159c1acd87e89a8783e9a2bbd4b98e
kht19684 <kht19684@gmail.com>
2024-08-28 17:52:11 +0900

    240828_section4_52수강

---
+++ 02_minigame-app/App.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+import StartGameScreen from "./screens/StartGameScreem";
+
+export default function App() {
+    return <StartGameScreen />;
+}
+
+const styles = StyleSheet.create({});
